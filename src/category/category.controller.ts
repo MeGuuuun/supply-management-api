58b0806d3c01cf758e65471ffa93774dc3968cfd
MeGuuuun <pjm991012@gmail.com>
2025-08-06 03:55:14 +0900

    feat: 카테고리 컨트롤러에 swagger 데코레이터 추가

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,13 +1,17 @@
-import {Controller, Get, Post, Body, Query, Put, Patch, Param, Delete} from '@nestjs/common';
+import {Controller, Get, Post, Body, Query, Patch, Param, Delete} from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiParam, ApiQuery } from '@nestjs/swagger';
+
 import { CategoryService} from "./category.service";
 import { Category } from "./category.entity";
 import {CategoryRequestDto, CategoryResponseDto} from "./category.dto";
 
+@ApiTags("카테고리 API")
 @Controller('categories')  // URL 경로 접두사
 export class CategoryController {
     constructor(private readonly categoryService: CategoryService) {}
 
     // 새 카테고리 등록
+    @ApiOperation({ summary: '새 카테고리 생성' })
     @Post()
     async addCategory(@Body() categoryRequestDto: CategoryRequestDto): Promise<Category> {
         console.log("controller - ", categoryRequestDto.name)
@@ -15,6 +19,9 @@ export class CategoryController {
     }
 
     // 전체 카테고리 목록 조회(10개씩 조회)
+    @ApiQuery({ name: 'page', description: '페이지 번호' })
+    @ApiQuery({ name: 'limit', description: '한 페이지에 보여줄 항목 수' })
+    @ApiOperation({ summary: '전체 카테고리 목록 조회' })
     @Get()
     async getAllCategories(
         @Query('page') page = 1,
@@ -24,6 +31,8 @@ export class CategoryController {
     }
 
     // 카테고리 갱신
+    @ApiParam({ name: 'categoryId', description: '갱신할 카테고리 ID' })
+    @ApiOperation({ summary: '카테고리 갱신' })
     @Patch(':categoryId')
     async updateCategory(
         @Param('categoryId') id: string,
@@ -33,6 +42,8 @@ export class CategoryController {
     }
 
     // 카테고리 삭제
+    @ApiParam({ name: 'categoryId', description: '삭제할 카테고리 ID' })
+    @ApiOperation({ summary: '카테고리 삭제' })
     @Delete(':categoryId')
     async deleteCategory(@Param('categoryId') id: string){
         return this.categoryService.deleteCategory(id);
--- src/category/category.dto.ts
+++ src/category/category.dto.ts
@@ -1,9 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CategoryRequestDto {
+    @ApiProperty({ example: '학용품', description: '카테고리 이름'})
     name: string;
 }
 
 export class CategoryResponseDto {
+    @ApiProperty({ example: 'uuid', description: '카테고리 id'})
     category_id: string;
+
+    @ApiProperty({ example: '학용품', description: '카테고리 이름'})
     name: string;
 
     // Partial<T> 사용하면 전달받는 객체에 필드 별 값이 없어도 에러가 나지 않음
