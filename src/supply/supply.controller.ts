e3c9635f87dfcdf10e3966444229d23610f0fea8
MeGuuuun <pjm991012@gmail.com>
2025-08-06 04:57:54 +0900

    feat: swagger 추가

--- src/supply/supply.controller.ts
+++ src/supply/supply.controller.ts
@@ -1,13 +1,17 @@
 import {Controller, Get, Post, Body, Param, Query} from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiParam, ApiQuery } from '@nestjs/swagger';
+
 import { SupplyService } from './supply.service';
 import { SupplyRequestDto } from "./supply.dto";
 import { Supply } from "./supply.entity";
 
+@ApiTags("비품 API")
 @Controller('supplies')  // URL 경로 접두사
 export class SupplyController {
     constructor(private readonly supplyService: SupplyService) {}
 
     // 새 비품 등록
+    @ApiOperation({ summary: '새 비품 등록' })
     @Post()
     async addSupply(@Body() supplyRequestDto: SupplyRequestDto): Promise<Supply> {
         return this.supplyService.addSupply(supplyRequestDto);
@@ -16,6 +20,10 @@ export class SupplyController {
     // 전체 비품 목록 조회(Redis 사용)
 
     // 특정 카테고리에 속한 비품 조회
+    @ApiParam({name: 'categoryId', description: '카테고리 id'})
+    @ApiQuery({ name: 'page', description: '페이지 번호' })
+    @ApiQuery({ name: 'limit', description: '한 페이지에 보여줄 항목 수' })
+    @ApiOperation({ summary: '특정 카테고리에 속한 비품 조회' })
     @Get(':categoryId')
     async getSuppliesByCategory(
         @Param('categoryId') id: string,
--- src/supply/supply.dto.ts
+++ src/supply/supply.dto.ts
@@ -1,14 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class SupplyRequestDto {
+    @ApiProperty({ example: '연필', description: '비품 이름'})
     name: string;
+
+    @ApiProperty({ example: 10 , description: '비품 수량'})
     quantity: number;
+
+    @ApiProperty({ example: 'uid', description: '카테고리 id'})
     categoryId: string;
 }
 
 export class SupplyResponseDto {
+    @ApiProperty({ example: 'uid', description: '비품 id'})
     supply_id: string;
+
+    @ApiProperty({ example: '연필', description: '비품 이름'})
     name: string;
+
+    @ApiProperty({ example: 10 , description: '비품 수량'})
     quantity: number;
+
+    @ApiProperty({ example: '사용 가능', description: '비품 상태'})
     status: string;
+
+    @ApiProperty({ example: 'uid', description: '카테고리 id'})
     categoryId: string;
 
     constructor(supply: Partial<SupplyResponseDto>) {
